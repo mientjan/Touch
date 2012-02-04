42fdf5784d902bdbc85d87aa8c04a4d113d449e2
Mient-jan Stelling <mientjan.stelling@gmail.com>
2012-02-04 20:10:22 +0100

    adding source

---
+++ source/Touch.js
@@ -0,0 +1,18 @@
+var Touch = new Class({
+	Implements: [Events,Options],
+	
+	options:{
+		
+	},
+	
+	initialize:function(el, options){
+		
+	}
+});
+
+Touch.Vector = new Class({
+	
+});
+
+
+// Touch.Scroll = new Class({});
